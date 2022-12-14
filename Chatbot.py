55bdbeab5315bc3b85805f76f8aac28cd72422ef
Adarsha Masalkar <97529973+adarsha-masalkar@users.noreply.github.com>
2022-12-14 18:01:44 +0530

    Cleaned up the code.

--- Chatbot.py
+++ Chatbot.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import nltk
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,59 +7,37 @@ import string
 import warnings
 warnings.filterwarnings('ignore')
 
-
-# In[2]:
-
-
 with open('chatbot.txt', 'r') as file:
     raw_chat = file.read().lower()
 
-
-# In[3]:
-
-
 sentence_tokens = nltk.sent_tokenize(raw_chat)
-
-
-# In[4]:
-
 
 word_tokens = nltk.word_tokenize(raw_chat)
 
-
-# In[5]:
-
-
 lemmatizer = nltk.stem.WordNetLemmatizer()
-
-
-# In[6]:
 
 
 def lemTokens(tokens):
     return [lemmatizer.lemmatize(token) for token in tokens]
 
+
 remove_punctuation = dict((ord(p), None) for p in string.punctuation)
+
 
 def lemNormalize(text):
     return lemTokens(nltk.word_tokenize(text.lower().translate(remove_punctuation)))
 
 
-# In[7]:
-
-
 greeting_input = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
 greeting_output = ['hi', 'hey', 'hi there', 'hello', 'hi, how are you?']
+
 
 def greeting(sentence):
     for word in sentence.split():
         if word.lower() in greeting_input:
             return random.choice(greeting_output)
-
-
-# In[8]:
-
-
+        
+        
 def response(request):
     bot_response = ''
     sentence_tokens.append(request)
@@ -83,9 +55,6 @@ def response(request):
     return bot_response
 
 
-# In[9]:
-
-
 print('Chatbot Online\n')
 while True:
     user = str(input('> ')).lower()
@@ -99,4 +68,3 @@ while True:
             print('Bot: ', end='')
             print(response(user))
             sentence_tokens.remove(user)
-
